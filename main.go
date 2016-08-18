0251980d8fe8f6082da8ce2b567808d2b0c0bfbe
cenan <me@cenanozen.com>
2016-08-18 15:55:12 +0300

    Open browser during startup

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/cenan/mergen/web"
+	"github.com/skratchdot/open-golang/open"
 )
 
 func main() {
 	port := flag.Int("port", 7777, "server port")
 	flag.Parse()
+	open.Run(fmt.Sprintf("http://localhost:%d", *port))
 	web.StartServer(*port)
 }
